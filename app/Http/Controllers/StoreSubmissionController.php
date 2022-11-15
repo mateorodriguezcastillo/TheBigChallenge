d0d2c726282844f832211b5e800b6177c4e17910
mateorodriguezcastillo <mateo.rodriguez@lightit.io>
2022-11-15 12:38:08 -0300

    feat: store submission

---
+++ app/Http/Controllers/StoreSubmissionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSubmissionRequest;
+use App\Transformers\SubmissionTransformer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class StoreSubmissionController extends Controller
+{
+    public function __invoke(StoreSubmissionRequest $request): JsonResponse
+    {
+        $submission = $request->user()->submissions()->create($request->validated());
+        return responder()
+            ->success($submission, SubmissionTransformer::class)
+            ->respond();
+    }
+}
---
+++ app/Models/Submission.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Submission extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'title',
+        'info',
+        'symptoms',
+        'status',
+    ];
+
+    protected $attributes = [
+        'status' => 'pending',
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+}
