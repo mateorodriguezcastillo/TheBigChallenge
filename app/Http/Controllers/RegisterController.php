f288a916a7a40e9f3618c8d603df527f53cfafaa
mateorodriguezcastillo <mateo.rodriguez@lightit.io>
2022-11-23 11:31:07 -0300

    feat: verify email

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
-use Illuminate\Auth\Events\Registered;
 use App\Models\Role;
+use App\Notifications\VerifyEmail;
 use App\Transformers\UserTransformer;
 use Illuminate\Http\JsonResponse;
 
-class RegisterController
+class RegisterController extends Controller
 {
     public function __invoke(RegisterRequest $request): JsonResponse
     {
-        $role = Role::find($request->validated()['role_id']);
+        $role = Role::where('name', $request->validated()['role'])->firstOrFail();
         $user = $role->users()->create($request->validated());
         $token = $user->createToken($request->ip())->plainTextToken;
-        event(new Registered($user));
+        $user->notify(new VerifyEmail);
         return responder()
             ->success($user, UserTransformer::class)
             ->meta(['token' => $token])
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\DownloadPrescriptionController;
 use App\Http\Controllers\StoreSubmissionController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\VerifyEmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,5 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/submission', StoreSubmissionController::class)->name('submission.store')->middleware('auth:sanctum');
 Route::post('login', LoginController::class)->name('user.login')->middleware('guest');
 Route::post('register', RegisterController::class)->name('user.register')->middleware('guest');
+Route::get('email/verify/{user}/{hash}', VerifyEmailController::class)->name('verification.verify')->middleware('guest');
 
 
